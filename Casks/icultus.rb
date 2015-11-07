8ba696d13388801d82e65bcac8073460f648952d
Randy <randypriv@gmail.com>
2015-11-07 21:06:20 +0800

    remove the comment in casks file

--- Casks/icultus.rb
+++ Casks/icultus.rb
@@ -5,7 +5,7 @@ cask :v1 => 'icultus' do
   url 'https://github.com/djyde/iCultus/releases/download/v1.0.1/iCultus-1.0.1-darwin-x64.zip'
   name 'iCultus'
   homepage 'http://djyde.github.io/iCultus'
-  license :mit    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :mit
 
   app 'iCultus.app'
 end
